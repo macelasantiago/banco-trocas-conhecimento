09a0b57bd53507efd3259a14f1425354f31f9f41
BeatrizS97 <bibia.wildestdreams@gmail.com>
2026-02-22 14:54:14 -0300

    feat(auth): integra rotas de registro e login na AP

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import pessoaRoutes from "./pessoa.routes.js";
 import conhecimentoRoutes from "./conhecimento.routes.js";
+import authRoutes from "./auth.routes.js"; 
 
 const routes = Router();
 
+routes.use(authRoutes);        
 routes.use(pessoaRoutes);
 routes.use(conhecimentoRoutes);
 
